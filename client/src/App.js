4ac2a47a3e577fd37fa962e529303fd643d8fd3c
Oren Zion <orenzion89@gmail.com>
2021-10-19 13:11:33 +0300

    tarted creating a nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,11 @@ import React, {useState, useEffect} from 'react'
 
 function App() {
 
-  const [data, setData] = useState([{}])
-  useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
-
   return (
-    <div>
-      {(typeof data.members === "undefined") ? (
-        <p>Loading...</p>
-      ) : (
-        data.members.map((member,i) => (
-          <p key={i}>{member}</p>
-        ))
-      )
-      }
+    <div className="App">
+      hi
     </div>
-  )
+  );
 }
 
 export default App
