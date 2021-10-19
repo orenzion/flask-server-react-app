e87c210310f8c051f49444715de5023e3c105aa8
Oren Zion <orenzion89@gmail.com>
2021-10-19 22:59:37 +0300

    created nav bar

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './Components/NavbarComponent';
 
 
 function App() {
   return (
     <div className="App">
-        Hello
+        <NavbarComponent/>
     </div>
   );
 }
