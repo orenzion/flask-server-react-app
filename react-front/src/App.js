b3e2a582362954ccb6c411dc81a80d2fab999e2f
Oren Zion <orenzion89@gmail.com>
2021-10-20 00:09:29 +0300

    added home and about components + routes

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './Components/NavbarComponent';
+import Home from './Components/Home'
+import About from './Components/About'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
         <NavbarComponent/>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home/>
+            </Route>
+            <Route exact path="/About">
+              <About/>
+            </Route>
+          </Switch>
+        </div>
     </div>
+    </Router>
   );
 }
 
--- react-front/src/Components/NavbarComponent.js
+++ react-front/src/Components/NavbarComponent.js
@@ -7,12 +7,12 @@ export default class NavbarComponent extends Component {
             <div>
                 <Navbar bg="dark" variant={"dark"} expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">Oren Zion</Navbar.Brand>
+                    <Navbar.Brand href="/">Oren Zion</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#About">About</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/About">About</Nav.Link>
                         <NavDropdown title="Projects" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Project 1</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Project 2</NavDropdown.Item>
