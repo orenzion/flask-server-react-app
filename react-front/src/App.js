b9776a4a9013b5018478a652a13cb546aaccc757
Oren Zion <orenzion89@gmail.com>
2021-10-21 11:20:40 +0300

    started to add iris form validation

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,31 +1,32 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComponent from './Components/NavbarComponent';
-import Home from './Components/Home'
-import About from './Components/About'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Iris from './Components/Iris';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarComponent from "./Components/NavbarComponent";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Iris from "./Components/Iris";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-        <NavbarComponent/>
+      <div className="App">
+        <NavbarComponent />
         <div className="content">
-          <Switch>
-            <Route exact path="/">
-              <Home/>
-            </Route>
-            <Route exact path="/about">
-              <About/>
-            </Route>
-            <Route exact path="/iris">
-              <Iris/>
-            </Route>
-          </Switch>
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/iris">
+                <Iris />
+              </Route>
+            </Switch>
+          </div>
         </div>
-    </div>
+      </div>
     </Router>
   );
 }
--- react-front/src/Components/IrisForm.js
+++ react-front/src/Components/IrisForm.js
@@ -1,31 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
 function IrisForm() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
-    <div>
-      <Form>
-        <Form.Group className="mb-3" controlId="sepal_length">
-          <Form.Label>Sepal Length</Form.Label>
-          <Form.Control type="text" placeholder="Enter Sepal Length" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="sepal_width">
-          <Form.Label>Sepal Width</Form.Label>
-          <Form.Control type="text" placeholder="Enter Sepal Width" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="petal_length">
-          <Form.Label>Petal Length</Form.Label>
-          <Form.Control type="text" placeholder="Enter Petal Length" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="petal_width">
-          <Form.Label>Petal Width</Form.Label>
-          <Form.Control type="text" placeholder="Enter Petal Width" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Predict
-        </Button>
-      </Form>
-    </div>
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="sepal_length">
+        <Form.Label>Sepal Length</Form.Label>
+        <Form.Control type="text" placeholder="Enter Sepal Length" required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="sepal_width">
+        <Form.Label>Sepal Width</Form.Label>
+        <Form.Control type="text" placeholder="Enter Sepal Width" required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="petal_length">
+        <Form.Label>Petal Length</Form.Label>
+        <Form.Control type="text" placeholder="Enter Petal Length" required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="petal_width">
+        <Form.Label>Petal Width</Form.Label>
+        <Form.Control type="text" placeholder="Enter Petal Width" required />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Predict
+      </Button>
+    </Form>
   );
 }
 
